d032c705d7da5b589cccc4df2e3feabbd79b7c7e
seansamu <seansamu.code@gmail.com>
2025-03-21 22:57:26 -0400

    created tile component

---
+++ app/src/component/tiles.jsx
@@ -0,0 +1,14 @@
+function Tile() {
+    const style = {
+        width: "100px",
+        height: "80px",
+        backgroundColor: "black",
+        borderRadis: "5px",
+        color: "yellow"
+    }
+
+    return (
+        <div className="style">Algo</div>
+    );
+}
+export default Tile
